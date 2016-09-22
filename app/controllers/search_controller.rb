8fa8e14a385e36d7f72d569e8a2eacff033783ff
Jorge <rodri0315@gmail.com>
2016-09-22 00:37:30 -0400

    Add search link to the navbar

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,7 @@ class SearchController < ApplicationController
     if params[:q].nil?
       @agencies = []
     else
-      @agencies = Agency.search( params[:q])
+      @agencies = Agency.search(params[:q])
     end
-    # unless params[:query].blank? 
-    #   @agencies = Agency.search( params[:query] )
-    # end
   end
 end
