b664b433290b639b21e50c2d352edbdac6ff079e
nand09 <osortof@yahoo.com>
2016-09-26 23:54:37 -0400

    added edit, delete, update with links to each action. about and home pages changed.

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,6 +1,7 @@
 class AgenciesController < ApplicationController
 
 	def index
+		@agencies = Agency.all
 	end
 
 	def show
@@ -21,6 +22,27 @@ class AgenciesController < ApplicationController
 		end
 	end
 
+	def edit
+		@agency = Agency.find(params[:id])
+	end
+
+	def update
+		@agency = Agency.find(params[:id])
+		if @agency.update_attributes(agency_params)
+			redirect_to @agency
+		else
+			render  'edit'
+		end
+	end
+
+	def destroy
+		Agency.find(params[:id]).destroy
+		flash[:notice] = "Successfully deleted agency."
+		redirect_to new_agency_url
+	end
+
+
+private
 	def agency_params
 		params.require(:agency).permit(:name, :address, :city, :state, :zipcode)
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  root to: 'agencies#index'
   resources :agencies
   devise_for :users
 end
