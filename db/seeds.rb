b7cfb6673b42dda8d8f18fe6d5e0543dc60bf5bb
Jorge Rodriguez <rodri0315@gmail.com>
2017-03-29 22:46:13 -0400

    Edit seeds file to remove creating an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+
+
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
+
+
+
 # Environment variables (ENV['...']) can be set in the file .env file.
 
 # Agency.create!([{
