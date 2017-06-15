3b0d69a0b162db5d650251eab0b21771c19e5a89
Jorge Rodriguez <rodri0315@gmail.com>
2017-06-14 22:42:51 -0400

    Change local db to pg, add search/pg gem, fix search form

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,7 @@ class Category < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates_uniqueness_of :name
+
+  include PgSearch
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314021801) do
+ActiveRecord::Schema.define(version: 20170612210115) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name"
@@ -51,6 +55,15 @@ ActiveRecord::Schema.define(version: 20170314021801) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.string   "searchable_type"
+    t.integer  "searchable_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -66,8 +79,8 @@ ActiveRecord::Schema.define(version: 20170314021801) do
     t.datetime "updated_at",                          null: false
     t.string   "name"
     t.integer  "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "website_types", force: :cascade do |t|
@@ -83,8 +96,10 @@ ActiveRecord::Schema.define(version: 20170314021801) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "url"
-    t.index ["agency_id"], name: "index_websites_on_agency_id"
-    t.index ["website_type_id"], name: "index_websites_on_website_type_id"
+    t.index ["agency_id"], name: "index_websites_on_agency_id", using: :btree
+    t.index ["website_type_id"], name: "index_websites_on_website_type_id", using: :btree
   end
 
+  add_foreign_key "websites", "agencies"
+  add_foreign_key "websites", "website_types"
 end
