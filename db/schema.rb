5972b52f28d566c2ff6c40385ea8097263fe6c07
Jorge Rodriguez <rodri0315@gmail.com>
2017-10-10 12:07:10 -0400

    Update gems and rails version, fix last migration

--- db/schema.rb
+++ db/schema.rb
@@ -16,88 +16,88 @@ ActiveRecord::Schema.define(version: 20170612210115) do
   enable_extension "plpgsql"
   enable_extension "unaccent"
 
-  create_table "agencies", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "zipcode"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.float    "latitude"
-    t.float    "longitude"
-    t.string   "contact"
-    t.string   "phone"
-    t.string   "description"
-    t.string   "email"
-    t.string   "descripcion"
+  create_table "agencies", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "contact"
+    t.string "phone"
+    t.string "description"
+    t.string "email"
+    t.string "descripcion"
   end
 
-  create_table "agency_categories", force: :cascade do |t|
+  create_table "agency_categories", id: :serial, force: :cascade do |t|
     t.integer "agency_id"
     t.integer "category_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "categoria"
-    t.string   "fa_name"
+    t.string "categoria"
+    t.string "fa_name"
   end
 
-  create_table "faqs", force: :cascade do |t|
-    t.string   "question"
-    t.text     "answer"
-    t.string   "pregunta"
-    t.text     "respuesta"
+  create_table "faqs", id: :serial, force: :cascade do |t|
+    t.string "question"
+    t.text "answer"
+    t.string "pregunta"
+    t.text "respuesta"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "pg_search_documents", force: :cascade do |t|
-    t.text     "content"
-    t.string   "searchable_type"
-    t.integer  "searchable_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
+  create_table "pg_search_documents", id: :serial, force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.integer "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name"
-    t.integer  "role"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "website_types", force: :cascade do |t|
-    t.string   "name"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "icon"
+    t.string "name"
+    t.integer "role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "websites", force: :cascade do |t|
-    t.integer  "agency_id"
-    t.integer  "website_type_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "url"
-    t.index ["agency_id"], name: "index_websites_on_agency_id", using: :btree
-    t.index ["website_type_id"], name: "index_websites_on_website_type_id", using: :btree
+  create_table "website_types", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "icon"
+  end
+
+  create_table "websites", id: :serial, force: :cascade do |t|
+    t.integer "agency_id"
+    t.integer "website_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "url"
+    t.index ["agency_id"], name: "index_websites_on_agency_id"
+    t.index ["website_type_id"], name: "index_websites_on_website_type_id"
   end
 
   add_foreign_key "websites", "agencies"
