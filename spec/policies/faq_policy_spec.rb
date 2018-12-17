38ae4e642c7dc4d09fd897a5fa7ce142b94e24e6
Cait Gorevin <caitlin.gorevin@gmail.com>
2018-12-17 12:19:15 -0500

    WIP

---
+++ spec/policies/faq_policy_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe FaqPolicy do
+
+  subject { described_class }
+  let(:current_user) { create :user }
+  let(:admin) { create :user, :admin, email: "conectateadmin@example.com" }
+
+  # We want all users to be able to access the index and show pages for our FAQs
+  # Applies to both admins and regular users
+  permissions :index?, :show? do
+    it 'allows access if not an admin' do
+      expect(subject).to permit(current_user)
+    end
+
+    it 'allows access if an admin' do
+      expect(subject).to permit(admin)
+    end
+  end
+
+  # These permissions are unique to admins only. We want to prevent regular users from accessing them.
+  permissions :update?, :create?, :destroy? do
+    it 'denies access if not an admin' do
+      expect(subject).not_to permit(current_user)
+    end
+    it 'allows access if an admin' do
+      expect(subject).to permit(admin)
+    end
+  end
+
+end
