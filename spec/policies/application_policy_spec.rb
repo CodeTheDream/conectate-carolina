e00f14c3486940cef119c14a6849e8b6df4a009f
Cait Gorevin <caitlin.gorevin@gmail.com>
2018-12-17 13:05:45 -0500

    Writing application policy test

---
+++ spec/policies/application_policy_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationPolicy do
+
+  subject { described_class }
+  let(:current_user) { create :user }
+  let(:record) { create :record }
+
+  # Users are not permitted access to the following actions
+  permissions :index?, :create?, :update?, :destroy?, :new?, :edit? do
+    it 'does not allow access' do
+      expect(subject).not_to permit(current_user)
+    end
+  end
+
+end
