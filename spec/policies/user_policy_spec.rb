a5d4fc340251359dde9066dec792608e34e7dbaa
Cait Gorevin <caitlin.gorevin@gmail.com>
2019-01-03 12:33:25 -0500

    Complete User policy test

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -6,14 +6,25 @@ RSpec.describe UserPolicy do
   let(:current_user) { create :user }
   let(:admin) { create :user, :admin, email: "conectateadmin@example.com" }
 
-  # Only admins are permitted to access index and show
-  permissions :index?, :show? do
+  # Only admins are permitted to access index
+  permissions :index? do
     it 'does not allow access if not an admin' do
       expect(subject).not_to permit(current_user)
     end
 
     it 'allows access if an admin' do
       expect(subject).to permit(admin)
+    end
+  end
+
+  # Only admins and the current user looking at themselves can access show
+  permissions :show? do
+    it 'allows access if an admin' do
+      expect(subject).to permit(admin, current_user)
+    end
+
+    it 'allows access if viewing self' do
+      expect(subject).to permit(current_user, current_user)
     end
   end
 
