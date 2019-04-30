223cadbc0f7b4ceddadd529a89eba299c59ad25e
Narayan Rai <narayanrai97@gmail.com>
2019-04-29 21:54:14 -0400

    Add GET /agencies based on category_id

--- app/controllers/api/v1/agencies_controller.rb
+++ app/controllers/api/v1/agencies_controller.rb
@@ -1,3 +1,9 @@
 class Api::V1::AgenciesController < ApplicationController
-  
+  def index
+    #list of agencies based on 'category_id'
+    @category = Category.find(params[:category_id])
+    @agencies = Agency.search_name(@category.name)
+    render json: @agencies
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+	namespace :api do
+		namespace :v1 do
+			resources :agencies
+		end
+	end
 
 	scope ":locale", locale:  /en|es/ do
 		devise_for :users
