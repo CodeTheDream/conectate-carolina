159e929b259c6fc3b4de422666aecd179b629eb9
Milton R <miltoncrestrepo@gmail.com>
2019-05-02 00:26:20 +0000

    faqs api test

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
 	namespace :api do
         namespace :v1 do
             resources :faqs
-        end
-        
-		namespace :v1 do
 	    	resources :categories, only: [:index]
     	end
     end
---
+++ spec/requests/faqs_api.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+ RSpec.describe "Faqs API", type: :request do
+    let!(:category) { create_list(:categories_api, 10) }
+
+  describe 'GET /faqs' do
+    before { get '/api/v1/faqs' }
+
+    it 'returns faqs' do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200)
+    end
+  end 
+    
+end
