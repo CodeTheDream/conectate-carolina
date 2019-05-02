5a91fee911d58c70a93cb596ef5f88652621496b
Narayan Rai <narayanrai97@gmail.com>
2019-05-02 12:51:06 -0400

    Complete GET /agencies endpoint with 'updated_since' params

--- app/controllers/api/v1/agencies_controller.rb
+++ app/controllers/api/v1/agencies_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::AgenciesController < ApplicationController
   def index
-    #list of agencies based on 'category_id'
-    category_id = params[:category_id]
-    @agencies = Agency.includes(:agency_categories).where(agency_categories: { category_id: category_id })
-    render json: @agencies
-  end
+    @agencies = Agency.all
 
+    # updated_since parameter
+    if params[:updated_since].present?
+      updated_since = params[:updated_since]
+      date = Time.parse(updated_since)
+      @agencies = Agency.where('updated_at >= ?', date)
+    end
+
+    @agencies2= @agencies.map do |agency|
+      agency.new_agency_hash
+    end
+
+    render json: @agencies2, status: :ok
+  end
 end
