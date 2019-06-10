b56ea671f7740d3287b95bf410ade87d5ca9289c
Narayan Rai <narayanrai97@gmail.com>
2019-06-10 14:24:17 -0400

    Add more feature tests for messages link

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "Messages", type: :feature, js: true do
   let!(:message) { create :message, title: "Alert!" }
+  let!(:admin) { create :user, :admin, email: "admin@example.com", password: "foobar"}
 
   scenario "user sees message" do
     visit root_path(locale: 'en')
@@ -15,6 +16,14 @@ RSpec.feature "Messages", type: :feature, js: true do
     click_button 'my_x_button' #hides message
     click_link 'my_icon_link'   #re-displays message
     expect(page).to have_text "Alert!"
+  end
+
+  scenario "displays list of messages" do
+    signin('admin@example.com', 'foobar')
+    visit root_path(locale: 'en')
+    expect(page).to have_text('Messages') #actually wanted to test as a link (`have_link`)
+    click_link 'my_messages_link' #redirects to message index page
+    expect(page).to have_text "List of messages"
   end
 
 end
