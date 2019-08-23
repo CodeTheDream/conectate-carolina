da66ca7ac09668a9a3bd16480f4e4325232f1829
Narayan Rai <narayanrai97@gmail.com>
2019-08-23 13:10:14 -0400

    Add check to not allow a device to register multiple times

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -2,14 +2,18 @@ class Api::V1::DevicesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    @device = Device.create!(device_params)
-    json_response(@device, :created)
+    @device = Device.where(token: params[:device][:token]).first
+
+    if @device.blank?
+      @device = Device.create!(device_params)
+      json_response(@device, :created)
+    end
   end
 
 
   private
-  def device_params
-    # whitelist params
-   params.permit(:token)
-  end
+    def device_params
+      # whitelist params
+     params.permit(:token)
+    end
 end
