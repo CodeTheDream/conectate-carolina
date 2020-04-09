cbc538a22f2e816c6deae77e7f3fe11546fc3e40
Narayan Rai <narayanrai97@gmail.com>
2020-04-09 17:34:36 -0400

    Turn off asset precompile initialization

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module SAF
         request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
+
+    config.assets.initialize_on_precompile = false
   end
 
 end
