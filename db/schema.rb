43868535dab55b234e5ca5ac29c2bb44cc513c6a
Narayan Rai <narayanrai97@gmail.com>
2021-05-25 16:13:54 -0400

    Add county and nombre columns to agencies table

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_183324) do
+ActiveRecord::Schema.define(version: 2021_05_25_201118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2021_03_02_183324) do
     t.string "email"
     t.string "descripcion"
     t.string "mobile_phone"
+    t.string "county"
+    t.string "nombre"
     t.index ["name", "address", "city", "state", "zipcode"], name: "unique_index_on_agencies", unique: true
   end
 
