25ebfdb0400acb3cac354c823030d5bc987c3eb5
Narayan Rai <narayanrai97@gmail.com>
2021-08-31 10:14:58 -0400

    Add geocoding to agency update request model

--- app/models/agency_update_request.rb
+++ app/models/agency_update_request.rb
@@ -2,9 +2,17 @@ class AgencyUpdateRequest < ApplicationRecord
   belongs_to :agency
   validates :name, :submitted_by, :submitter_email, presence: true
 
+  geocoded_by :full_address
+  after_validation :geocode, if: ->(obj) { obj.full_address.present? && obj.address_changed? }
+
   def attributes_from_keys(*keys)
     keys.inject({}) do |hash_to_return, key|
       hash_to_return.merge(key => send(key))
     end
+  end
+
+  def full_address
+    sub_address = [address, city, state].compact.join(', ')
+    [sub_address, zipcode].compact.join(' ')
   end
 end
