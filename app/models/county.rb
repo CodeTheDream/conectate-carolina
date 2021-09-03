aa38ce573a50f156254815b759fb808804905a2b
Narayan Rai <narayanrai97@gmail.com>
2021-09-02 22:36:12 -0400

    Add uniqueness validation on county name

--- app/models/county.rb
+++ app/models/county.rb
@@ -2,4 +2,5 @@ class County < ApplicationRecord
   has_many :agency_counties
   has_many :agencies, through: :agency_counties
   validates :name, :state, presence: true
+  validates :name, uniqueness: { scope: :state, message: "should be unique to a state" }
 end
