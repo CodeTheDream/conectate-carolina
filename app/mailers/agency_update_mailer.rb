ba358b7c280e5fc9166bc248fc0a148730ba50e8
Narayan Rai <narayanrai97@gmail.com>
2021-10-06 14:29:35 -0400

    Add default cc in the mailer

--- app/mailers/agency_update_mailer.rb
+++ app/mailers/agency_update_mailer.rb
@@ -1,8 +1,10 @@
 class AgencyUpdateMailer < ApplicationMailer
+  default cc: -> { User.where(role: 'admin').pluck(:email) }
+
   def new_agency_update_email
     @agency = params[:agency]
     @aur = params[:agency_update_request]
 
-    mail(from: @agency.email, cc: 'admin@saf-unite.org', subject: 'You got a new organization update request!')
+    mail(from: @agency.email, to: 'admin@saf-unite.org', subject: 'You got a new organization update request!')
   end
 end
