b6fe24da2828b8e9bd7f0db68b26552f7f2c1c1d
Narayan Rai <narayanrai97@gmail.com>
2021-10-07 13:37:31 -0400

    Change from to reply_to to 'noreply@conectatecarolina.org'

--- app/mailers/agency_update_mailer.rb
+++ app/mailers/agency_update_mailer.rb
@@ -4,7 +4,7 @@ class AgencyUpdateMailer < ApplicationMailer
     @agency = params[:agency]
     @aur = params[:agency_update_request]
 
-    mail(from: @agency.email, to: 'admin@saf-unite.org', cc: (Proc.new { User.where(role: 'admin').pluck(:email) }).call, subject: 'You got a new organization update request!')
+    mail(reply_to: 'noreply@conectatecarolina.org', to: 'admin@saf-unite.org', cc: User.where(role: 'admin').pluck(:email), subject: 'You got a new organization update request!')
   end
 
   def agency_update_approval_email
