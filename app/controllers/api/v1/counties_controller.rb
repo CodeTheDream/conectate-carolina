45b7c21feffbf7dc9e8ee6adfd66f0c51fa1e5d0
Narayan Rai <narayanrai97@gmail.com>
2021-10-25 19:13:16 -0400

    Remove nested api response for counties endpoint

--- app/controllers/api/v1/counties_controller.rb
+++ app/controllers/api/v1/counties_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CountiesController < ApplicationController
 
   def index
     counties = County.all.map {|c| {id: c.id, name: c.name, state: c.state} }
-    render json: { counties: counties }, status: :ok
+    render json: counties, status: :ok
   end
 
 end
